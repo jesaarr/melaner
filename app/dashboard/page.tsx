55098c87d2d1b31bffa059abfc2c3e9f4b6874b2
Mert <qmert2090@gmai.com>
2026-01-13 14:18:39 +0300

    yogi model ismi guncellendi2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -91,7 +91,7 @@ const Dashboard = () => {
         return;
       }
 
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
